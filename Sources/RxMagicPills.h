8677ebcabc1f1bcb0b46aa039334d41455e7fddb
Sebastián Varela <sebastian.varela@masmovil.com>
2019-10-03 12:15:38 +0200

    Add empty project

---
+++ Sources/RxMagicPills.h
@@ -0,0 +1,19 @@
+//
+//  MagicPills.h
+//  MagicPills
+//
+//  Created by Sebastián Varela Basconi on 27/03/2019.
+//  Copyright © 2019 BQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MagicPills.
+FOUNDATION_EXPORT double RxMagicPillsVersionNumber;
+
+//! Project version string for MagicPills.
+FOUNDATION_EXPORT const unsigned char RxMagicPillsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagicPills/PublicHeader.h>
+
+
---
+++ Tests/ExampleTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+import Foundation
+import RxMagicPills
+
+class ExampleTests: XCTestCase {
+    func test_example() {
+        XCTAssert(true)
+    }
+}
